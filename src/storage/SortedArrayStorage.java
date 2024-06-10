8f091e41390fbc3ddd9494b8e729c82fc49822e0
gennadykudryavtsev <gennadykudryavtsev@gmail.com>
2024-06-10 21:18:14 +0300

    Lesson06 Iterator, Comparator and Inner Classes

--- src/storage/SortedArrayStorage.java
+++ src/storage/SortedArrayStorage.java
@@ -31,6 +31,6 @@ public class SortedArrayStorage extends AbstractArrayStorage{
     @Override
     protected Object getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 }
