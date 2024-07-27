4422b99769442e4aee2489a5e7719ceb5c7518e6
gennadykudryavtsev <gennadykudryavtsev@gmail.com>
2024-07-27 23:33:56 +0300

    change printing catalogs

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -1,3 +1,5 @@
+package com.urise.webapp;
+
 import java.io.File;
 import java.util.Arrays;
 
@@ -8,26 +10,31 @@ public class MainFile {
         File file = new File("/Users/gennadykudryavtsev/Desktop/MyCode/MyCourseBJ/basejava");
         mainFile.printFiles(file);
     }
-
+    StringBuilder space = new StringBuilder("");
     public void printFiles(File file) {
 
         File[] lst = file.listFiles();
-        if (lst == null) return;
+        if (lst == null) {
+            return;
+        }
         Arrays.sort(lst);
         for (File value : lst) {
             if (value.isDirectory()) {
                 if (value.getName().equals(".git")) {
                     continue;
                 }
-                System.out.println(value.getName());
+
+                System.out.println(space + value.getName());
+                space.append("   ");
                 printFiles(value);
             }
         }
         for (File value : lst) {
             if (!value.isDirectory()) {
-                System.out.println(" " + value.getName());
+                System.out.println(space + "   " + value.getName());
+
             }
         }
-        System.out.println();
+        space.replace(0, space.length(), "");
     }
 }
