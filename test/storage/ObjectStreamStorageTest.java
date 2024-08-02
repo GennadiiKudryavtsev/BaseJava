e65524c87117dcfc4bebfb04065b537d9a5f5d8e
gennadykudryavtsev <gennadykudryavtsev@gmail.com>
2024-08-02 13:17:48 +0300

    rename class in the import

--- test/storage/ObjectStreamStorageTest.java
+++ test/storage/ObjectStreamStorageTest.java
@@ -1,11 +1,11 @@
 package storage;
 
-import com.urise.webapp.storage.AbstractFileStorage;
+import com.urise.webapp.storage.FileStorage;
 import com.urise.webapp.storage.strategy.ObjectStreamStorage;
 
 class ObjectStreamStorageTest extends AbstractArrayStorageTest {
 
     public ObjectStreamStorageTest() {
-        super(new AbstractFileStorage(STORAGE_DIR, new ObjectStreamStorage()));
+        super(new FileStorage(STORAGE_DIR, new ObjectStreamStorage()));
     }
 }
